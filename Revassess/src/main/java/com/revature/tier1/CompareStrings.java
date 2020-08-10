ac637606bc3005ff0a63ae93b339f35691210446
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-10 13:15:02 -0500

    Initial push to test

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-        return false;
+        if(s1.length != s2.length) {
+        	return false;
+        }
+        for(int i = 0; i < s1.length; i++) {
+        	if(s1.charAt(i) != s2.charAt(i)) {
+        		return false;
+        	}
+        }
+        return true;
     }
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,11 @@
 package com.revature.tier1;
 
 public class User {
+	public int id;
+	public String firstName;
+	public String lastName;
+	public String username;
+	public String password;
+	public String role;
+	
 }
