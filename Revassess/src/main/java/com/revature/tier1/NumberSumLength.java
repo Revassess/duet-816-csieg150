d49a156251f8b144869d13912bf9ef48732a17c4
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-10 13:50:54 -0500

    passed all tests

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,17 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		String number = String.valueOf(num);
+		int length = number.length();
+		int sum = 0;
+		for(int i = 0; i < number.length(); i++) {
+			String digitAtI = String.valueOf((number.charAt(i)));
+			int value = (int) Math.pow(Double.parseDouble(digitAtI), length);
+			sum += value;
+		}
+		if(sum == num) {
+			return true;
+		}
 		return false;
 	}
 }
